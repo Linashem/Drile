10ee709c6d254eeaa82e2169578495d9f4be66d5
Linashem <lina.shemeneva@yandex.ru>
2023-02-19 06:17:33 +0300

    bestsellers

--- src/Components/HomeContent/index.tsx
+++ src/Components/HomeContent/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Products } from '../Bestsellers/Products';
 
 export const HomeContent = () => {
   return (
-    <h1>HOME</h1>
-  )
-}
+    <>
+      <Products />
+    </>
+  );
+};
--- src/Components/common/Header/index.tsx
+++ src/Components/common/Header/index.tsx
@@ -5,34 +5,34 @@ import style from './Header.module.scss';
 
 export const Header = () => {
   return (
-  <header className=''>
-    <div>
-      <img src="/img/svg/logo.svg" alt="" />
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <Link to={routes.home}>Home</Link>
-        </li>
-        <li>
-          <Link to={routes.shop}>Shop</Link>
-        </li>
-        <li>
-          <Link to={routes.blog}>Blog</Link>
-        </li>
-        <li>
-          <Link to={routes.contact}>Contact</Link>
-        </li>
-      </ul>
-    </nav>
-    <div>
-      <Link to={routes.cart}>
-        <img src="/img/svg/cart.svg" alt="" />
-      </Link>
-      <Link to={routes.auth}>
-        <img src="/img/svg/auth.svg" alt="" />
-      </Link>
-    </div>
-  </header>
+    <header className={style.header}>
+      <div>
+        <img src="/img/svg/logo.svg" alt="" />
+      </div>
+      <nav>
+        <ul className={style.header_list}>
+          <li className={style.header_list_item}>
+            <Link className={style.header_list_item_link} to={routes.home}>Home</Link>
+          </li>
+          <li className={style.header_list_item}>
+            <Link className={style.header_list_item_link} to={routes.shop}>Shop</Link>
+          </li>
+          <li className={style.header_list_item}>
+            <Link className={style.header_list_item_link} to={routes.blog}>Blog</Link>
+          </li>
+          <li className={style.header_list_item}>
+            <Link className={style.header_list_item_link} to={routes.contact}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={style.header_navbar} >
+        <Link to={routes.cart}>
+          <img src="/img/svg/cart.svg" alt="" />
+        </Link>
+        <Link to={routes.auth}>
+          <img src="/img/svg/auth.svg" alt="" />
+        </Link>
+      </div>
+    </header>
   );
 };
