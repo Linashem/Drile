44303df024dfdfed3b5e4a7433cb8d92e1c149e8
Linashem <lina.shemeneva@yandex.ru>
2023-02-24 19:48:26 +0300

    shop is main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Home } from './Containers/Home';
 import { Routes, Route } from 'react-router-dom';
 import { routes } from './Helper/Conatants/routes';
 import { Pagewrapper } from './Components/common/PageWrapper';
--- src/Containers/Home/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { HomeContent } from '../../Components/HomeContent'
-
-export const Home = () => {
-  return (
-    <HomeContent/>
-  )
-}
